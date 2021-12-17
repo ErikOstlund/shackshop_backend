ee81081fa9a1d96e317a39e8eca9ec7ad5ec07aa
ErikOstlund <eostlund74@gmail.com>
2021-12-17 10:23:07 -0600

    Basic Express Server Setup
    And created package.json file.

--- index.js
+++
@@ -1,6 +0,0 @@
-console.log('Hello Shack Shop!');
-
-const one = 1;
-const two = 2;
-
-console.log(`1 + 2 = ${one + two}`);
---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 9000;
+
+app.get('/', (req, res) => res.send('Shack Shop!'));
+
+app.listen(port);
+
+console.log(`[app]: Ready at http://localhost:${port}`);
