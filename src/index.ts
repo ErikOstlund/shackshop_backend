9fd14b879e8c90f33aca17f7dcf39d7421d689cd
ErikOstlund <eostlund74@gmail.com>
2022-03-27 18:19:51 -0500

    Connect to MongoDB
    Setup MongoDB Cluster
    Setup Listing Interface and Interface for Collection<Listing>
    Setup ENV Variables using dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,34 @@
-import express from 'express';
+import express, { Application } from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+import { connectDatabase } from './database';
 import { typeDefs, resolvers } from './graphql';
 
-// Express
-const app = express();
-const port = 9000;
+const mount = async (app: Application) => {
+	const db = await connectDatabase();
+	// Apollo
+	const server = new ApolloServer({
+		typeDefs,
+		resolvers,
+		context: () => ({ db }),
+		plugins: [ApolloServerPluginLandingPageGraphQLPlayground]
+	});
 
-// Apollo
-const server = new ApolloServer({
-	typeDefs,
-	resolvers,
-	plugins: [ApolloServerPluginLandingPageGraphQLPlayground]
-});
+	// Need startServer() for apollo-server-express 3.x
+	const startServer = async () => {
+		await server.start();
+		// pass in the express app and the endpoint for where our GraphQL API should live.
+		server.applyMiddleware({ app, path: '/api' });
+	};
 
-// Need startServer() for apollo-server-express 3.x
-const startServer = async () => {
-	await server.start();
-	// pass in the express app and the endpoint for where our GraphQL API should live.
-	server.applyMiddleware({ app, path: '/api' });
+	startServer();
+
+	app.listen(process.env.PORT);
+
+	console.log(`[app]: Ready at http://localhost:${process.env.PORT}`);
+
+	const listings = await db.listings.find({}).toArray();
+	console.log(listings);
 };
 
-startServer();
-
-app.listen(port);
-
-console.log(`[app]: Ready at http://localhost:${port}`);
+mount(express());
